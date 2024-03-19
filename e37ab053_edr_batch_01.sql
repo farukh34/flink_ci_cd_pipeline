b8e367915be3c7e6396e10552fc6874adaea47c3
farukh34 <126668701+farukh34@users.noreply.github.com>
2024-03-19 11:54:24 +0530

    Update e37ab053_edr_batch_01.sql

--- e37ab053_edr_batch_01.sql
+++ e37ab053_edr_batch_01.sql
@@ -615,5 +615,5 @@ insert into FlinkOut SELECT
        'tacticidlist' VALUE ARRAY['TA0003'],
        'techniqueidlist' VALUE ARRAY['T1546.003']
 
-  )) FROM EdrTable WHERE (`organization`.`id`='e37ab053') AND ((REGEXP_EXTRACT(cast(`EventID` as VARCHAR), '^19$') is not null OR REGEXP_EXTRACT(cast(`EventID` as VARCHAR), '^20$') is not null OR REGEXP_EXTRACT(cast(`EventID` as VARCHAR), '^21$') is not null) );
+  )) FROM EdrTable WHERE (`organization`.`id`='e37ab053') AND ((REGEXP_EXTRACT(cast(`event.id` as VARCHAR), '^19$') is not null OR REGEXP_EXTRACT(cast(`event`.`id` as VARCHAR), '^20$') is not null OR REGEXP_EXTRACT(cast(`event`.`id` as VARCHAR), '^21$') is not null) );
 END;
